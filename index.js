64f01810cf7bbf68d7284c3e75bbf9a74f623a19
Juneid Teklu <te.juneid@gmail.com>
2024-03-01 15:03:07 -0600

    Add styling for visibility and create pseudocode to determine dimensions of square divs

---
+++ index.js
@@ -0,0 +1,27 @@
+/*
+ Store the dimensions of etch-sqaures (700px) into a variable named etchSquaresDimension.
+ Get a number from the user and perform the following calculation: 
+    - Dvide 700px by the number provided by user (with var name num);
+    - Store that value into variable divSquareDimension;
+    - create num * num amount of squares with the height and width of divSquareDimension;
+    - appendChild each of those squares into the etch-squares container;
+    - flex-wrap must be applied to etch-squares container for this to work properly;
+    - etch-squares container dimensions need to remain fixed for this to work properly;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ */

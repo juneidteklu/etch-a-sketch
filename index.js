843178291f7315bc6ce1320442455424d5cd9ea0
Juneid Teklu <te.juneid@gmail.com>
2024-03-02 12:34:20 -0600

    Add if statement that checks if input value is less than 100 and greater than 0

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-/*
- Store the dimensions of etch-sqaures (700px) into a variable named etchSquaresDimension.
- Get a number from the user and perform the following calculation: 
-    - Dvide 700px by the number provided by user (with var name num);
-    - Store that value into variable individualSquareDimension;
-    - create num * num amount of squares with the height and width of divSquareDimension;
-    - appendChild each of those squares into the etch-squares container;
-    - flex-wrap must be applied to etch-squares container for this to work properly;
-    - etch-squares container dimensions need to remain fixed for this to work properly;
-*/
-
 const etchSquaresContainerDimension = 700;
 const apply = document.querySelector(".apply");
 const reset = document.querySelector(".reset");
@@ -18,28 +7,25 @@ const etchSquaresContainer = document.querySelector(".etch-squares-container");
 apply.addEventListener("click", createIndividualSquares);
 
 function createIndividualSquares(){
-    const etchContainerDimension = document.querySelector("input").value;
-    let totalNumOfSquares = etchContainerDimension * etchContainerDimension;
-    let individualSquareDimension = etchSquaresContainerDimension / etchContainerDimension;
+        const etchContainerDimension = document.querySelector("input").value;
+        let totalNumOfSquares = etchContainerDimension * etchContainerDimension;
+        let individualSquareDimension = etchSquaresContainerDimension / etchContainerDimension;
+    if(etchContainerDimension >= 1 && etchContainerDimension <= 100){
+        if(etchSquaresContainer.firstElementChild){
+            deleteEtchSquares();
+        }
+        
+        let i = 0;
+        while(i<totalNumOfSquares){
+            const etchSquare = document.createElement("div");
+            etchSquare.classList.add("etch-square");
+            etchSquare.style.height= individualSquareDimension + "px";
+            etchSquare.style.width= individualSquareDimension + "px";
 
-    /* If etch-squares-container has children, run the deleteEtchSquares() to remove them
-    before populating with the new number of squares.
-        If there are no children, run the while loop below
-     */
-    if(etchSquaresContainer.firstElementChild){
-        deleteEtchSquares();
-    }
-    
-    let i = 0;
-    while(i<totalNumOfSquares){
-        const etchSquare = document.createElement("div");
-        etchSquare.classList.add("etch-square");
-        etchSquare.style.height= individualSquareDimension + "px";
-        etchSquare.style.width= individualSquareDimension + "px";
+            etchSquaresContainer.appendChild(etchSquare);
 
-        etchSquaresContainer.appendChild(etchSquare);
-
-        i++;
+            i++;
+        }
     }
     
 }
